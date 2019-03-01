1b448de940f20063fd5c7c980b8c7ba527b069d6
G-Lauz <gabriellauzier77@gmail.com>
2019-03-01 09:58:22 -0500

    Redefine _get_autheticated_service in EmailReader()

--- emailreader.py
+++ emailreader.py
@@ -2,10 +2,11 @@
 from getpass import getpass
 import imaplib as imap
 
+import pickle
+import os.path
+from google.auth.transport.requests import Request
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-
-from oauth2 import *
 
 class GmailReader():
 
@@ -19,22 +20,33 @@ class GmailReader():
         self._API_SERVICE_NAME = "gmail"
         self._API_VERSION = "v1"
 
+        self._credentials = None
         self._service = None
 
         self.service = self._get_authenticated_service()
 
-
     def _get_authenticated_service(self):
-        flow = InstalledAppFlow.from_client_secrets_file(
-            self.CLIENT_SECRET,  scopes=self.SCOPES)
-        credentials = flow.run_local_server(host='localhost',
-            port=8080,
-            authorization_prompt_message='Redirection...',
-            success_message="""L'authentification est terminer vous pouvez fermer cette page""",
-            open_browser=True)
-        print("Credentials: ",credentials, type(credentials))
+        if os.path.exists('../token.pickle'):
+            with open('../token.pickle', 'rb') as token:
+                self.credentials = pickle.load(token)
+        #Login si il n'y a pas d'indentifiant valide
+        if not self.credentials or not self.credentials.valid:
+            if self.credentials and self.credentials.expired and self.credentials.refresh_token:
+                self.credentials.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    self.CLIENT_SECRET,  scopes=self.SCOPES)
+                self.credentials = flow.run_local_server(host='localhost',
+                    port=8080,
+                    authorization_prompt_message='Redirection...',
+                    success_message="""L'authentification est terminer vous pouvez fermer cette page""",
+                    open_browser=True)
+                #Enregistrer l'identifiant pour la prochaine exécution
+                with open('../token.pickle', 'wb') as token:
+                    pickle.dump(self.credentials, token)
+        #Changer?
         return build(self.API_SERVICE_NAME,
-            self.API_VERSION, credentials=credentials)
+            self.API_VERSION, credentials=self.credentials)
 
     #Test
     def readMail(self):
@@ -68,11 +80,17 @@ class GmailReader():
     def _get_service(self):
         return self._service
 
+    def _get_credentials(self):
+        return self._credentials
+
     #===========================================================================
     # set
     #===========================================================================
     def _set_service(self, service):
         self._service = service
+
+    def _set_credentials(self, credentials):
+        self._credentials = credentials
 
     #===========================================================================
     # Propriété
@@ -83,7 +101,8 @@ class GmailReader():
     API_VERSION = property(fget=_get_API_VERSION)
 
     service = property(fget=_get_service, fset=_set_service)
+    credentials = property(fget=_get_credentials, fset=_set_credentials)
 
 if __name__ == "__main__":
     er = GmailReader()
-    #er.readMail()
+    er.readMail()
